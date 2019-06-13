4873354b51fee70dc2b7792a3b00855e45c50554
Frederick Mendoza <frederick.mendoza@projecticeland.net>
2019-06-13 16:54:23 -0400

    adding tests

--- test_files/file_tests/test_parse_file.py
+++ test_files/file_tests/test_parse_file.py
@@ -27,23 +27,29 @@ class test_parse_file(object):
 
     def test_happy_path(self):
         print "*** Happy path test ***"
-        self.parse_file("../files/testFile.txt")
+        filename = "../files/testFile.txt"
+        assert self.parse_file(filename), "Expecting file to be found and valid. Check if file %s actually exists with correct format" % filename
 
     def test_empty_file(self):
         print "*** Empty file test ***"
-        self.parse_file("../files/testEmptyFile.txt")
+        filename = "../files/testEmptyFile.txt"
+        assert self.parse_file(filename), "Expecting file to be found and valid. Check if file %s actually exists with correct format" % filename
 
     def test_sentences_in_file(self):
         print "*** Sentences in File test ***"
-        self.parse_file("../files/testSentencesFile.txt")
+        filename = "../files/testSentencesFile.txt"
+        assert self.parse_file(filename), "Expecting file to be found and valid. Check if file %s actually exists with correct format" % filename
 
     def test_file_does_not_exist(self):
         print "*** File Does Not Exist test ***"
-        self.parse_file("../files/fileDoesNotExist.txt")
+        filename = "../files/testFileDoesNotExist.txt"
+        #filename = "../files/testFile.txt"        #enable this line to check if test will fail if file exists (testing your method actually works if file actually exists)
+        assert self.parse_file(filename) is False, "Expecting file DOES NOT EXIST. File %s is found" % filename
 
     def test_file_invalid_format(self):
         print "*** Invalid file format test ***"
-        self.parse_file("../files/testImageFile.png")
+        filename = "../files/testImageFile.png"
+        assert self.parse_file(filename) is False, "Expecting file DOES NOT EXIST. File %s is found with valid format" % filename
 
     def parse_file(self, test_file):
         check_file = self.pfm.check_file_type(test_file)
@@ -54,8 +60,10 @@ class test_parse_file(object):
             print "Largest words: %s" % largest_words
             reversed_words = self.pfm.reverse_words(largest_words)
             print "Reversed words: %s" % reversed_words
+            return True
         else:
             print check_file
+            return False
 
 if __name__ == '__main__':
     tfm = test_parse_file()
